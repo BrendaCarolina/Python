53477a6c1e634168ea50d09bbef4f1a23b2ff8e6
Brenda Carolina Melo Dos Santos <40414880@redecorp.br>
2023-05-23 13:21:22 -0300

    2

--- Alura/Alura_01.py
+++ Alura/Alura_01.py
@@ -14,9 +14,16 @@ numero_secreto = int(42)
 chute = int(input('Digite seu palpite: '))
 print('Você digitou: ', chute)
 
-if (numero_secreto == chute):
+acertou = numero_secreto == chute
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if (acertou):
     print('Você acertou!')
 else:
-    print('Você errou, tente novamente! ')
+    if(maior):
+        print('Você errou. Seu chute foi maior que o numero secreto ')
+    elif (menor):
+        print('Você errou. Seu chute foi menor que o número secreto ')
 
 print('Fim de jogo')
---
+++ Alura/Alura_02.py
@@ -0,0 +1,10 @@
+usuario = input(str('Informe o usuário do sistema: '))
+
+if(usuario == 'Flavio'):
+    print(f'Seja bem-vindo {usuario}')
+elif (usuario == 'Nico'):
+    print(f'Seja bem-vindo {usuario}')
+elif(usuario == 'Douglas'):
+    print(f'Seja bem-vindo {usuario}')
+else:
+    print('Usuário não identificado!')
