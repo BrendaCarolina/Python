7b2b67b1758cb60fc24262d33ce8fce6bfb4f7ce
Brenda Carolina Melo Dos Santos <brenda.csantos@telefonica.com>
2023-07-08 22:23:40 -0300

    modulos

--- Alura/forca.py
+++ Alura/forca.py
@@ -1,3 +1,10 @@
-print('**********************************')
-print('Bem vindo ao Jogo da forca')
-print('**********************************')
+def jogar(): #função que irá permitir que chame esse jogo em outro código
+
+    print('**********************************')
+    print('Bem vindo ao Jogo da forca')
+    print("**********************************")
+
+    print('Fim do jogo!')
+
+if(__name__ == "__main__"):
+    jogar()
--- Alura/jogos.py
+++ Alura/jogos.py
@@ -1,14 +1,26 @@
-print('**********************************')
-print('******Escolha seu jogo!******')
-print('**********************************')
+#trazendo os códigos dos jogos pra esse código
 
-print('Forca (1) ou Advinhação (2)? ')
+import forca
+import advinhacao
 
-jogo = int(input('Digite sua opção: '))
+def escolhe_jogo():
 
-if(jogo == 1):
-    print('Jogando forca')
-elif (jogo == 2):
-    print('Jogando adivinhação')
-else:
-    print('Valor incorreto. Escolha entre as opções disponíveis')
+    print('**********************************')
+    print('******Escolha seu jogo!******')
+    print('**********************************')
+
+    print('Forca (1) ou Advinhação (2)? ')
+
+    jogo = int(input('Digite sua opção: '))
+
+    if(jogo == 1):
+        print('Jogando forca')
+        forca.jogar()   #chamando o jogo forca
+    elif (jogo == 2):
+        print('Jogando adivinhação')
+        advinhacao.jogar()  #chamando o jogo advinhação
+    else:
+        print('Valor incorreto. Escolha entre as opções disponíveis')
+
+if(__name__ == '__main__'):
+    escolhe_jogo()
